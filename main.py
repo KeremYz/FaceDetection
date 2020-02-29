720a1d5b917420f8fcdb5c24950381bd32e0dc03
KeremYz <keremyzc10@gmail.com>
2020-02-29 18:13:08 +0300

    add comments

--- main.py
+++ main.py
@@ -7,22 +7,24 @@ cap = cv2.VideoCapture(0)
 screenName = 'Camera'
 cv2.namedWindow(screenName, cv2.WINDOW_NORMAL);
 while True:
-    zartZurt, colorfulScreen = cap.read()
+    zartZurt, colorfulScreen = cap.read()#read camera
     colorfulScreen = imutils.resize(colorfulScreen, width=1500, height=120)
     colorfulScreen = imutils.rotate(colorfulScreen, angle=0)
-    grayScreen = cv2.cvtColor(colorfulScreen, cv2.COLOR_BGR2GRAY)
+    grayScreen = cv2.cvtColor(colorfulScreen, cv2.COLOR_BGR2GRAY)#converted gray for detection
 
-    casc = faceCascade.detectMultiScale(grayScreen, 1.3, 2)
+    casc = faceCascade.detectMultiScale(grayScreen, 1.3, 2)#detect
+
     for (x, y, w, h) in casc:
-        cv2.rectangle(colorfulScreen, (x + 5, y + 5), (x + w - 5, y + h - 5), (0, 255, 0), 2)
+        cv2.rectangle(colorfulScreen, (x + 5, y + 5), (x + w - 5, y + h - 5), (0, 255, 0), 8)#draw a rectangle around face
 
-    cv2.imshow(screenName, colorfulScreen)
+    cv2.imshow(screenName, colorfulScreen)#show on screen
+    
     key = cv2.waitKey(1)
     if key == ord('x'):  # press x for quit
-        print("Gidiyorsun demek ")
+        print("Shutting down... ")
         break
-    if key == ord('f'):
-        print("TAM EKRAN ")
+    if key == ord('f'): # press f for full screen
+        print("Full Screen ")
         cv2.setWindowProperty(screenName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 cap.release()
